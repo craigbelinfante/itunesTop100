359801b96c17287bb148beb1a43a9cf905f915c2
craig <craigbasketball@aol.com>
2022-02-04 23:47:02 -0800

    starter code for project

---
+++ itunesTop100/itunesTop100/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  itunesTop100
+//
+//  Created by Craig Belinfante on 2/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ itunesTop100/itunesTop100/itunesTop100App.swift
@@ -0,0 +1,17 @@
+//
+//  itunesTop100App.swift
+//  itunesTop100
+//
+//  Created by Craig Belinfante on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct itunesTop100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
