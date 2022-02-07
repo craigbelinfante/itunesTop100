bf4df8f505380e04b0e4a159cc33c598dd40b731
craig <craigbasketball@aol.com>
2022-02-06 23:51:06 -0800

    created album view file and changed it to albumlistview on launch

---
+++ itunesTop100/itunesTop100/AlbumListView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumListView.swift
+//  itunesTop100
+//
+//  Created by Craig Belinfante on 2/6/22.
+//
+
+import SwiftUI
+
+struct AlbumListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumListView()
+    }
+}
--- itunesTop100/itunesTop100/itunesTop100App.swift
+++ itunesTop100/itunesTop100/itunesTop100App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct itunesTop100App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AlbumListView()
         }
     }
 }
